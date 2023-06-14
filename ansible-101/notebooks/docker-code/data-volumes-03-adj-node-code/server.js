a6e0a164ea6ec202ec6000deb27f4841b5b2ebca
d8o <domi.cutrupi@gmail.com>
2023-06-14 11:15:03 +0200

    edited version

--- ansible-101/notebooks/docker-code/data-volumes-03-adj-node-code/server.js
+++ ansible-101/notebooks/docker-code/data-volumes-03-adj-node-code/server.js
@@ -36,8 +36,7 @@ app.post('/create', async (req, res) => {
     if (exists) {
       res.redirect('/exists');
     } else {
-      await fs.copyFile(tempFilePath, finalFilePath);
-      await fs.unlink(tempFilePath);
+      await fs.rename(tempFilePath, finalFilePath);
       res.redirect('/');
     }
   });
